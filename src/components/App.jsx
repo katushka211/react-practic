3efc9abbcdc50b34d219faecbc9549e9fd85fbb9
Kateryna Verveda <katushka211@gmail.com>
2023-04-16 16:46:07 +0200

    add 01-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { RecipeList } from './RecipeList';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <RecipeList items={[1, 2, 3, 4, 5]} />
+    </>
   );
 };
