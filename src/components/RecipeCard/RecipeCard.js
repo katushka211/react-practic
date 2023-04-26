1cad73f91478ca6762b3189608df7b3e63f22b07
Kateryna Verveda <katushka211@gmail.com>
2023-04-26 14:53:07 +0200

    update propTypes

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -17,6 +17,18 @@ import {
 import { ImageModal } from 'components/ImageModal/ImageModal';
 
 export class RecipeCard extends Component {
+  static propTypes = {
+    onDelete: PropTypes.func.isRequired,
+    item: PropTypes.shape({
+      image: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      time: PropTypes.number.isRequired,
+      servings: PropTypes.number.isRequired,
+      calories: PropTypes.number.isRequired,
+      difficulty: PropTypes.oneOf(['easy', 'medium', 'hard']),
+    }).isRequired,
+  };
+
   state = {
     selectedImg: null,
   };
@@ -86,14 +98,3 @@ export class RecipeCard extends Component {
     );
   }
 }
-
-RecipeCard.propTypes = {
-  item: PropTypes.shape({
-    image: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    time: PropTypes.number.isRequired,
-    servings: PropTypes.number.isRequired,
-    calories: PropTypes.number.isRequired,
-    difficulty: PropTypes.oneOf(['easy', 'medium', 'hard']),
-  }).isRequired,
-};
